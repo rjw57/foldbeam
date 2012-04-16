ede33447c3a71f4ebf5161f92ec6564df9e00d97
Rich Wareham <rjw57@cam.ac.uk>
2012-04-16 23:14:31 +0100

    make use of cache-dir option for generating the samples

--- sample-output/gen_samples.sh
+++ sample-output/gen_samples.sh
@@ -3,20 +3,21 @@
 THISDIR="${PWD}/`dirname $0`"
 WIDTH=1024
 RENDER="${THISDIR}/../bin/foldbeam-render"
+CACHE="${THISDIR}/tilecache"
 
 cd "${THISDIR}"
 
 # Generate a world map using equirectangular projection
-${RENDER} --output world-equirectangular.tiff -w ${WIDTH} -l -180 -r 180 -t 89 -b -89
+${RENDER} --output world-equirectangular.tiff -w ${WIDTH} -l -180 -r 180 -t 89 -b -89 --cache-dir "${CACHE}"
 # Generate a world map using mercator projection
 ${RENDER} --output world-mercator.tiff -w ${WIDTH} \
-    -l -20000000 -t 16000000 -r 20000000 -b -14000000 --epsg 3395
+    -l -20000000 -t 16000000 -r 20000000 -b -14000000 --epsg 3395 --cache-dir "${CACHE}"
 # Generate the US National Atlas equal area projection
-${RENDER} --output us.tiff -w ${WIDTH} -l -3000000 -t 2500000 -r 3600000 -b -4700000 --epsg 2163
+${RENDER} --output us.tiff -w ${WIDTH} -l -3000000 -t 2500000 -r 3600000 -b -4700000 --epsg 2163 --cache-dir "${CACHE}"
 # Generate a UK OS national grid map with 1 pixel == 1 km
-${RENDER} --output uk.tiff -w 700 -l 0 -r 700000 -t 1300000 -b 0 --epsg 27700
+${RENDER} --output uk.tiff -w 700 -l 0 -r 700000 -t 1300000 -b 0 --epsg 27700 --cache-dir "${CACHE}"
 # Generate a UK OS national grid map centred on Big Ben
-${RENDER} --output bigben.tiff -w ${WIDTH} -l 530069 -t 179830 -r 530469 -b 179430 --epsg 27700
+${RENDER} --output bigben.tiff -w ${WIDTH} -l 530069 -t 179830 -r 530469 -b 179430 --epsg 27700 --cache-dir "${CACHE}"
 
 for i in *.tiff; do convert "$i" "`basename $i tiff`jpg"; done
 rm *.tiff
