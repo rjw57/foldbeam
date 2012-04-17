a59fdbf59657903e73059988834016e122c5a425
Rich Wareham <rjw57@cam.ac.uk>
2012-04-17 16:55:07 +0100

    document foldbeam.core

--- src/foldbeam/core.py
+++ src/foldbeam/core.py
@@ -1,8 +1,26 @@
+"""
+Core geometric operations
+=========================
+
+Mechanisms for converting axis-aligned bounding boxes (:py:class:`Envelope`) and polygonal boundaries
+(:py:class:`Boundary`) from one spatial reference system to another. It is used internally to convert the regions
+represented by tiles between spatial references.
+
+"""
+
 import json
 from TileStache.Geography import Point
 from osgeo import ogr, gdal
 
 def boundary_from_envelope(envelope):
+    """Construct a :py:class:`Boundary` from an :py:class:`Envelope`. The boundary is the bounding box which encloses
+    the envelope.
+
+    :param envelope: the envelope to convert into a boundary
+    :type envelope: :py:class:`Envelope`
+
+    """
+
     wkt = 'POLYGON ((%s))' % (
             ','.join(['%f %f' % x for x in [
                 (envelope.left,envelope.top),
@@ -17,21 +35,32 @@ def boundary_from_envelope(envelope):
     return Boundary(geom)
 
 class ProjectionError(RuntimeError):
+    """Error raised when projection from one spatial reference to another has failed.
+
+    :param message: a description of the error
+    :type message: str
+    
+    """
+
     def __init__(self, message):
         super(ProjectionError, self).__init__(message)
 
 class Boundary(object):
+    """A polygon defining an enclosing boundary for a region. A boundary *always* has an associated spatial reference.
+
+    Generally you'll want to use the :py:func:`boundary_from_envelope` function to construct a boundary unless you have
+    specific needs.
+
+    :param geometry: boundary geometry
+    :type geometry: :py:class:`ogr.Geometry` with polygon type
+
+    .. py:data:: geometry
+
+        An underlying instance of ogr.Geometry which specifies the boundary polygon.
+
+    """
+
     def __init__(self, geometry):
-        """A single polygon defining an enclosing boundary for a region.
-
-        :param geometry: boundary geometry
-        :type geometry: obr.Geometry with polygon type
-
-        .. py:data:: geometry
-            An underlying instance of ogr.Geometry which specifies the boundary polygon.
-
-        """
-
         if geometry is None:
             raise ValueError('geometry cannot be None')
         if geometry.GetSpatialReference() is None:
@@ -40,15 +69,57 @@ class Boundary(object):
         self.geometry = geometry
 
     def envelope(self):
+        """Calculate the bounding axis-aligned envelope which entirely contains this boundary.
+        
+        Since a boundary is an arbitrary polygon and an envelope is only an axis-aligned bounding box, the returned
+        envelope will also contain point not within this boundary.
+
+        :rtype: :py:class:`Envelope`
+
+        """
+
         l,r,b,t = self.geometry.GetEnvelope()
         return Envelope(l,r,t,b,self.geometry.GetSpatialReference())
 
     def contains_point(self, x, y):
+        """Convenience function to test if a point is contained within this boundary.
+
+        :param x: the point's x-co-ordinate
+        :type x: float
+        :param y: the point's y-co-ordinate
+        :type y: float
+        :rtype: bool
+
+        """
+
         pt = ogr.Geometry(ogr.wkbPoint)
         pt.AddPoint_2D(x,y)
         return self.geometry.Contains(pt)
 
     def transform_to(self, other_spatial_reference, src_seg_len=None, dst_seg_len=None):
+        """Transform this boundary into another spatial reference.
+
+        :param other_spatial_reference: the destination spatial reference
+        :type other_spatial_reference: :py:class:`osr.SpatialReference`
+        :param src_seg_len: the maximum length of a boundary segment in the source spatial reference
+        :type src_seg_len: float or None
+        :param dst_seg_len: the maximum length of a boundary segment in the detination spatial reference
+        :type dst_seg_len: float or None
+        :rtype: :py:class:`Boundary`
+
+        This is a subtle function. The boundary is defined as a polygon of linear segments but, of course, lines do not
+        necessarily map to lines in a projection. This function may optionally segmentise the boundary before
+        transforming and simplify it afterwards.
+
+        If *src_seg_len* is not None, it specifies the maximum length in the source spatial reference for boundary
+        segments. If known, set this to the approximate length of a pixel in the source spatial reference.
+
+        If *dst_seg_len* is not None, it specifies the maximum length in the destination spatial reference used to
+        simplify the transformed boundary. If known, set this to the approximate length of a pixel in the destination
+        spatial reference.
+        
+        """
+
         geom = self.geometry.Clone()
         if src_seg_len is not None:
             geom.Segmentize(float(src_seg_len))
@@ -72,6 +143,15 @@ class Boundary(object):
         return 'Boundary(%s, %s)' % (self.geometry, self.geometry.GetSpatialReference().ExportToWkt())
 
 class Envelope(object):
+    """An axis-aligned bounding box in a particular spatial reference.
+
+    :param left: the left-most x co-ordinate
+    :param right: the right-most x co-ordinate
+    :param top: the top-most y co-ordinate
+    :param bottom: the bottom-most y co-ordinate
+
+    """
+
     def __init__(self, left, right, top, bottom, spatial_reference):
         self.left = left
         self.right = right
@@ -80,19 +160,44 @@ class Envelope(object):
         self.spatial_reference = spatial_reference
 
     def top_left(self):
+        """Obtain the top-left point for this envelope.
+
+        :rtype: :py:class:`TileStache.Geography.Point`
+
+        """
         return Point(self.left, self.top)
 
     def bottom_right(self):
+        """Obtain the bottom-right point for this envelope.
+
+        :rtype: :py:class:`TileStache.Geography.Point`
+
+        """
         return Point(self.right, self.botom)
 
     def offset(self):
-        """Return a tulple giving the offset from the top-left corner to the bottom right."""
+        """Return a tuple giving the offset from the top-left corner to the bottom right."""
         return (self.right - self.left, self.bottom - self.top)
 
     def size(self):
+        """Return a tuple giving the *absolute* offset from the top-left corner to the bottom right."""
         return map(abs, self.offset())
 
     def transform_to(self, other_spatial_reference, src_seg_len=None, dst_seg_len=None):
+        """Return a envelope which contains this envelope in a target spatial reference.
+
+        This is a convenience function which converts the envelope into a rectangular boundary, transforms it and
+        returns the envelope of the resulting boundary.
+
+        :param other_spatial_reference: the destination spatial reference
+        :type other_spatial_reference: :py:class:`osr.SpatialReference`
+        :param src_seg_len: the maximum length of a boundary segment in the source spatial reference
+        :type src_seg_len: float or None
+        :param dst_seg_len: the maximum length of a boundary segment in the detination spatial reference
+        :type dst_seg_len: float or None
+        :rtype: :py:class:`Envelope`
+
+        """
         return boundary_from_envelope(self). \
             transform_to(other_spatial_reference, src_seg_len, dst_seg_len).envelope()
 
