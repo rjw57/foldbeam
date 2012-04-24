d09e91f82a4e13a7825cc859e1525f6d8be4cae7
Rich Wareham <rjw57@cam.ac.uk>
2012-04-24 12:35:57 +0100

    added update documentation script

--- update_doch.sh
+++ update_doch.sh
@@ -2,9 +2,10 @@
 THISDIR=`dirname $0`
 cd ${THISDIR}
 
-DOCFILES=`pushd "docs/html/" >/dev/null; find .; popd >/dev/null`
-cp -r docs/html/* .
+DOCFILES=`pushd "docs/html/" >/dev/null; find . -type f; popd >/dev/null`
+cp -rv docs/html/* docs/html/.* .
 for f in ${DOCFILES}; do
+    echo "Adding ${f}"
     git add "${f}"
 done
 git commit -m "Automated commit on `date`"
