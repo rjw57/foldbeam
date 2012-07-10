a3ad60981e6673a2f7b7f41fc540d772f7427652
Rich Wareham <rjw57@cam.ac.uk>
2012-07-11 00:41:37 +0100

    added stub renderer object

---
+++ src/foldbeam/core.py
@@ -0,0 +1,7 @@
+class RendererBase(object):
+    def render(self, context):
+        """Called to render the object to the specified cairo context.
+
+        :param context: the cairo context to render this object to
+        """
+        pass
