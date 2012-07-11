1cfef2a623b957844e9e216da6637aa0a4a0f74a
Rich Wareham <rjw57@cam.ac.uk>
2012-07-11 19:17:26 +0100

    added urlib interceptor to tests which could be used to monkeypatch offline mode
    Making the test suite rely on an Internet connection is non-ideal and will result in the mapquest servers being
    hammered. Add in some infrastructure for a URL cache.

--- src/foldbeam/tests/renderer_tests.py
+++ src/foldbeam/tests/renderer_tests.py
@@ -1,6 +1,16 @@
+import logging
 import unittest
+import urllib2
 
 import cairo
+
+# Intercept urllib2's  urlopen function which is used by TileFetcher so that we can provide output from the cache
+_original_urlopen = urllib2.urlopen
+def _urlopen(url, *args, **kwargs):
+    global _original_urlopen
+    logging.info('Intercepted attempt to load URL: {0}'.format(url))
+    return _original_urlopen(url, *args, **kwargs)
+urllib2.urlopen = _urlopen
 
 from foldbeam.core import set_geo_transform
 from foldbeam.renderer import TileFetcher
@@ -15,15 +25,26 @@ class TestTileFetcher(unittest.TestCase):
         self.cr.set_source_rgba(0,0,0,0)
         self.cr.paint()
 
+    def centre_on_big_ben(self, width=500):
         # The EPSG:3857 co-ordinates of Big Ben, an easily identifiable landmark
         cx, cy = (-13871.6330672413, 6710328.3443702850)
-        width = 0.5e3 # 500m
-        height = float(width * sh) / float(sw)
-        set_geo_transform(self.cr, cx-0.5*width, cx+0.5*width, cy+0.5*height, cy-0.5*height, sw, sh)
+        height = float(width * self.surface.get_height()) / float(self.surface.get_width())
+        set_geo_transform(
+                self.cr,
+                cx-0.5*width, cx+0.5*width, cy+0.5*height, cy-0.5*height,
+                self.surface.get_width(), self.surface.get_height())
 
     def test_default(self):
+        self.centre_on_big_ben()
         renderer = TileFetcher()
         renderer.render(self.cr)
         output_surface(self.surface, 'tilefetcher_default')
         self.assertEqual(surface_hash(self.surface), 32199)
+
+    def test_aerial(self):
+        self.centre_on_big_ben(1000e3)
+        renderer = TileFetcher(url_pattern='http://oatile1.mqcdn.com/tiles/1.0.0/sat/{zoom}/{x}/{y}.jpg')
+        renderer.render(self.cr)
+        output_surface(self.surface, 'tilefetcher_aerial')
+        self.assertEqual(surface_hash(self.surface), 33548)
 
