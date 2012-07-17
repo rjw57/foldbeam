f24ded4af5c96804c8e1c8ba0f4597723919aee3
Rich Wareham <rjw57@cam.ac.uk>
2012-07-17 15:37:25 +0100

    tidy up the example server script

--- examples/run_server.sh
+++ examples/run_server.sh
@@ -2,7 +2,10 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
 
+HOST=localhost
+PORT=8080
+
 echo "Starting server... (requires gunicorn)"
-echo "After server started, visit file://${SCRIPT_DIR}/leaflet-example.html"
-exec gunicorn -b localhost:8080 -w 8 tilestache_provider_server:app
+echo "After server started, visit http://${HOST}:${PORT}/"
+exec gunicorn -b "${HOST}:${PORT}" -w 8 tilestache_provider_server:app
 
