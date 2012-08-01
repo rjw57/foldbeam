d196955678a23ef49714e4c097b678190d642b03
Rich Wareham <rjw57@cam.ac.uk>
2012-08-01 18:36:37 +0100

    setup.py: removie unised package: futures

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ install_requires = [
     # For more details, see:
     # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
     'filecache',
-    'futures',
     'gdal',
     'geoalchemy',
     'httplib2',
