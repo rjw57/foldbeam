3083c1de38b004227bfff1d11633af77fb8170a7
Rich Wareham <rjw57@cam.ac.uk>
2012-08-02 16:10:23 +0100

    do not include test module in coverage, only main code

--- run_tests_with_coverage.sh
+++ run_tests_with_coverage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-coverage run --include=foldbeam/*,tests/* ${DIR}/setup.py test && coverage html
+coverage run --include=foldbeam/* ${DIR}/setup.py test && coverage html
