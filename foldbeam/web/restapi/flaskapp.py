2958e3760b8a1a593b5d1db5da0fbf9244fffc3b
Rich Wareham <rjw57@cam.ac.uk>
2012-08-04 14:10:08 +0100

    support CORS in REST API - test suite now passes again

--- foldbeam/web/restapi/flaskapp.py
+++ foldbeam/web/restapi/flaskapp.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Flask
+from flask import Flask, request
 import mimerender
 
 mimerender = mimerender.FlaskMimeRender()
@@ -8,3 +8,12 @@ render_json = lambda **kwargs: json.dumps(kwargs)
 resource = mimerender(default='json', json=render_json)
 
 app = Flask(__name__)
+
+# Add an after request handler to support CORS
+def after_request(response):
+    if 'Origin' in request.headers:
+	response.headers['Access-Control-Allow-Origin'] = request.headers['Origin']
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
+    return response
+app.after_request(after_request)
